21f08e95d5879b9047d0d7860934384891b25cf3
ssekhonn <simrats472@deltalearns.ca>
2026-05-12 12:41:31 -0700

    Add regex for email address extraction

--- main.py
+++ main.py
@@ -19,3 +19,9 @@ phone_regex = re.compile(r'''(
     (\d{4})                          # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
 )''', re.VERBOSE)
+email_regex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+               # username
+    @                               # @ symbol
+    [a-zA-Z0-9.-]+                  # domain name
+    (\.[a-zA-Z]{2,4})               # dot-something
+)''', re.VERBOSE)

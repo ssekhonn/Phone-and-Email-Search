1599da6a284c372faa54563d513e96c90bed10f5
ssekhonn <simrats472@deltalearns.ca>
2026-05-12 12:39:20 -0700

    Add pseudocode for phone and email extraction functionality

---
+++ main.py
@@ -0,0 +1,9 @@
+''' 
+pseudocode:
+1. import re and pyperclip
+2. create regex for phone numbers and email addresses
+3. get text from clipboard
+4. extract phone numbers and email addresses from text
+5. join extracted phone numbers and email addresses into a string with newlines in between
+6. copy the extracted phone numbers and email addresses back to the clipboard
+'''

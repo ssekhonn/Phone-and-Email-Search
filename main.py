e6f2a79fec3642f4d997996b347b5a4f84d04f04
ssekhonn <simrats472@deltalearns.ca>
2026-05-12 12:40:59 -0700

    Add regex for phone number extraction

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ pseudocode:
 '''
 import re
 import pyperclip
+
+# create regex for phone numbers and email addresses
+phone_regex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?              # area code
+    (\s|-|\.)?                      # separator
+    (\d{3})                          # first 3 digits
+    (\s|-|\.)                        # separator
+    (\d{4})                          # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+)''', re.VERBOSE)
